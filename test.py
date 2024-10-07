38088c1415fd478082275d9e388a1ee5fb5b9cc9
federicoFrancesconi <ffedericofrancesconi@gmail.com>
2024-10-07 14:01:05 -0300

    test.py base

---
+++ test.py
@@ -0,0 +1,11 @@
+import pytest
+
+# Una gramática LL(1) sin recursión a derecha
+# Una gramática LL(1) con recursión a derecha
+# Una gramática LL(1) que incluya lambda en sus derivaciones
+# Una gramática LL(1) que no incluya lambda en sus derivaciones
+# Una gramática LL(1) con reglas de producción innecesarias.
+# Una gramática LL(1) con símbolos inaccesibles desde el axioma.
+# Una gramática LL(1) con no terminales no generativos.
+# Por cada uno de los incisos previos, una gramática que no sea LL(1).
+# Una gramática no LL(1) con recursión a izquierda

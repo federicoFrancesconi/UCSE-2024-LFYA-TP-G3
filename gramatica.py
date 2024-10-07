5850c6a4b6f2b735abec10eeefb193dfa1ab038a
federicoFrancesconi <ffedericofrancesconi@gmail.com>
2024-10-07 14:01:01 -0300

    gramatica.py base

---
+++ gramatica.py
@@ -0,0 +1,16 @@
+class Gramatica:
+    esLL1 = False
+
+    # Esta función debe implementar la lógica suficiente para que al imprimir en pantalla una instancia de este objeto
+    # a la que se le haya invocado dicho método previamente se muestre la gramática de la siguiente manera:
+    # por cada regla o producción mostrar dicha regla y a continuación los First, Follows y Selects correspondientes.
+    def setear(self, gramatica):
+        return
+    
+    # Devuelve true en caso de que la cadena se derive de la gramática y false en caso contrario. 
+    def evaluar_cadena(self, cadena):
+        return
+    
+    # Funcion para imprimir la gramatica
+    def __str__(self):
+        return ""

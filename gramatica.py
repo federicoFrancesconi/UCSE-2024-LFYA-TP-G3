ecf75f0f580a14b8f3776678526fa3ccb050d6db
Inwinkelried <santiagoinwinkelried@gmail.com>
2024-10-12 19:42:07 -0300

    add validacion gramaticas ambiguas

--- gramatica.py
+++ gramatica.py
@@ -2,6 +2,8 @@
 # Para agregar varios elementos a un set se usa .update
 
 from collections import defaultdict
+
+
 # Usamos defaultdict para:
 # * Generar keys automáticamente si agregamos y aún no están en el diccionario
 # * Poder tratar un diccionario como si fuese otra estructura de datos (list o set)
@@ -9,17 +11,20 @@ from collections import defaultdict
 def es_no_terminal(simbolo):
     return simbolo[0].isupper()
 
+
 def es_terminal(simbolo):
     return simbolo.islower() and simbolo != 'lambda'
+
 
 def es_lambda(simbolo):
     return simbolo == 'lambda'
 
-#Recibe una gramática y genera un diccionario de la forma {antecedente:[(consecuente),(consecuente)],}
+
+# Recibe una gramática y genera un diccionario de la forma {antecedente:[(consecuente),(consecuente)],}
 def generar_producciones(gramatica):
     reglas = gramatica.strip().split('\n')
     producciones = defaultdict(list)
-    
+
     for regla in reglas:
         regla = regla.strip()
         antecedente, consecuente = regla.split(':')
@@ -31,16 +36,17 @@ def generar_producciones(gramatica):
 
     return producciones
 
+
 # Calcula recursivamente los Firsts para cada NT
 def generar_firsts_para_no_terminal(no_terminal, gramatica_procesada, firsts_por_nt):
     # Condicion de corte: los Firsts ya fueron calculados
     if no_terminal in firsts_por_nt and firsts_por_nt[no_terminal]:
         return firsts_por_nt[no_terminal]
-    
+
     # Eliminamos duplicados metiendo los Firsts del NT en un set
     # TODO: ver si esto es necesario
     firsts_por_nt[no_terminal] = set()
-    
+
     # Recorremos las reglas del NT
     for consecuente in gramatica_procesada[no_terminal]:
         for simbolo in consecuente:
@@ -51,15 +57,16 @@ def generar_firsts_para_no_terminal(no_terminal, gramatica_procesada, firsts_por
                 # Aplicamos la recursión
                 first_value_nt = generar_firsts_para_no_terminal(simbolo, gramatica_procesada, firsts_por_nt)
                 firsts_por_nt[no_terminal].update(first_value_nt)
-                
+
                 # Si en los Firsts del NT está lambda, seguimos buscando
                 if 'lambda' not in first_value_nt:
                     break
             elif es_lambda(simbolo):
                 firsts_por_nt[no_terminal].add('lambda')
                 break
-    
+
     return firsts_por_nt[no_terminal]
+
 
 def generar_firsts(gramatica_procesada):
     firsts_por_nt = defaultdict(set)
@@ -104,8 +111,9 @@ def generar_firsts(gramatica_procesada):
                     # Regla 4: Si existe la posibilidad de que todos los caminos deriven en lambda, entonces lambda está en los firsts
                     firsts.add('lambda')
             firsts_por_regla[antecedente].append((tuple(consecuente), firsts))
-    
+
     return firsts_por_regla, firsts_por_nt
+
 
 def generar_follows(gramatica_procesada, firsts_por_nt):
     follows = defaultdict(set)
@@ -125,13 +133,13 @@ def generar_follows(gramatica_procesada, firsts_por_nt):
                     if es_no_terminal(simbolo):
                         # Si no es el último símbolo del consecuente (es decir: tiene algo a la derecha)
                         if indice + 1 < len(consecuente):
-                            siguiente = consecuente[indice+1]
+                            siguiente = consecuente[indice + 1]
                             if es_terminal(siguiente):
                                 # Si no lo añadi anteriormente, lo añado y aviso del cambio
                                 if siguiente not in follows[simbolo]:
-                                    follows[simbolo].add(siguiente)  
+                                    follows[simbolo].add(siguiente)
                                     cambios = True
-                            
+
                             elif es_no_terminal(siguiente):
                                 # Regla 2: Si un NT esta seguido de otro NT, los Firsts del último (menos lambda),
                                 # pertenecen a los Follows del primero
@@ -139,7 +147,7 @@ def generar_follows(gramatica_procesada, firsts_por_nt):
                                 firsts_siguiente_sin_lambda = firsts_siguiente - {'lambda'}
 
                                 # Solo agrego dichos Firsts si no los agregué aún
-                                if not firsts_siguiente_sin_lambda.issubset(follows[simbolo]): 
+                                if not firsts_siguiente_sin_lambda.issubset(follows[simbolo]):
                                     follows[simbolo].update(firsts_siguiente_sin_lambda)
                                     cambios = True
 
@@ -153,17 +161,18 @@ def generar_follows(gramatica_procesada, firsts_por_nt):
                         # Si no tiene nada a la derecha
                         else:
                             # Regla 3: Si llegaste hasta el final y seguís con lambda, podes tomar como follows los follows de tu superior
-                            if not follows[antecedente].issubset(follows[simbolo]):  
-                                follows[simbolo].update(follows[antecedente])  
+                            if not follows[antecedente].issubset(follows[simbolo]):
+                                follows[simbolo].update(follows[antecedente])
                                 cambios = True
     return follows
+
 
 def generar_select(gramatica_procesada, firsts, follows):
     # La estructura va a ser un defaultdict {A: [(consecuente, select), (consecuente, select)]}
     select = defaultdict(list)
     for antecedente in gramatica_procesada:
         for consecuente, firsts_consecuente in firsts[antecedente]:
-            #Por ahora los select de la regla van a ser los firsts del primer consecuente
+            # Por ahora los select de la regla van a ser los firsts del primer consecuente
             select_por_regla = set(firsts_consecuente)
             # Si tenes lambda, te llevas tus Follows en lugar de lambda
             if 'lambda' in firsts_consecuente:
@@ -172,30 +181,40 @@ def generar_select(gramatica_procesada, firsts, follows):
             select[antecedente].append((consecuente, select_por_regla))
     return select
 
+
 class Gramatica:
     esLL1 = False
     producciones = defaultdict(list)
     firsts = defaultdict(list)
     follows = defaultdict(set)
     select = defaultdict(list)
-                        
+
     # Esta función debe implementar la lógica suficiente para que al imprimir en pantalla una instancia de este objeto
     # a la que se le haya invocado dicho método previamente se muestre la gramática de la siguiente manera:
     # por cada regla o producción mostrar dicha regla y a continuación los First, Follows y Selects correspondientes.
     def setear(self, gramatica):
         self.producciones = generar_producciones(gramatica)
-        
+
         self.firsts, firsts_por_nt = generar_firsts(self.producciones)
-        
+
         # TODO: si no tengo lambda en ningun first, no tiene sentido que calcule los follows
         self.follows = generar_follows(self.producciones, firsts_por_nt)
-        
+
         self.select = generar_select(self.producciones, self.firsts, self.follows)
-    
-    # Devuelve true en caso de que la cadena se derive de la gramática y false en caso contrario. 
+        # Chequear si es gramatica ambigua:
+        # Si para dos o más producciones con el mismo antecedente, hay un simbolo repetido en el conjunto de los select, es una gramatica ambigua.
+        for antecedente in self.select:
+            select_simbolos = set()
+            for (consecuente, selects_produccion) in self.select[antecedente]:
+                if not select_simbolos.isdisjoint(selects_produccion):
+                    self.esLL1 = False
+                    return
+                select_simbolos.update(selects_produccion)
+
+    # Devuelve true en caso de que la cadena se derive de la gramática y false en caso contrario.
     def evaluar_cadena(self, cadena):
         return
-    
+
     # Funcion para imprimir la gramatica
     def __str__(self):
         resultado = ""
@@ -206,4 +225,3 @@ class Gramatica:
                 select_regla = [select_regla for regla, select_regla in self.select[antecedente] if regla == consecuente][0]
                 resultado = resultado + f"{antecedente} : {' '.join(consecuente)} [{', '.join(firsts_regla)}] [{', '.join(follows)}] [{', '.join(select_regla)}]\n"
         return resultado
-    
